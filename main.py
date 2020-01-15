fbcae986ad6fde2f2537f68fe89a1fd7fc6f7eb8
emelinely <59033583+emelinely@users.noreply.github.com>
2020-01-15 14:03:17 -0800

    clean up main.py

--- main.py
+++ main.py
@@ -24,65 +24,50 @@ snow_weather_conditions = ['light snow', 'Snow', 'Heavy snow', 'Sleet', 'Light s
 other_weather_conditions = [ 'Smoke', 'Haze', 'sand/ dust whirls',  'sand', 'dust', 'volcanic ash', 'squalls', 'tornado'] 
   
 complete_url = "http://api.openweathermap.org/data/2.5/weather?appid=a3e304c7b5f418e6e8463db911baabac&q=San Francisco"
+
 while True:  
-  # get method of requests module 
-  # return response object 
   response = requests.get(complete_url) 
-    
-  # json method of response object  
-  # convert json format data into 
-  # python format data 
   x = response.json() 
-
-    
-  # store the value of "weather" 
-  # key in variable z 
   z = x["weather"] 
-    
-  # store the value corresponding  
-  # to the "description" key at  
-  # the 0th index of z 
-  test_weather_condition = z[0]["description"] 
-    
-  # print following value
-  print(str(test_weather_condition)) 
+  weather_condition = z[0]["description"] 
+  print(str(weather_condition)) 
 
 
-  if test_weather_condition in possible_weather_conditions:
-    if test_weather_condition in clear_weather_conditions:
+  if weather_condition in possible_weather_conditions:
+    if weather_condition in clear_weather_conditions:
       print('Playing CLEAR weather conditions')
       a = "clear.wav"
-    elif test_weather_condition in heavy_rain_weather_conditions:
+    elif weather_condition in heavy_rain_weather_conditions:
       print('Playing HEAVY RAIN weather condition')
       a = "heavy_rain.wav"
-    elif test_weather_condition in rain_weather_conditions:
+    elif weather_condition in rain_weather_conditions:
       print('Playing RAIN weather condition')
       a = "rain.wav"
-    elif test_weather_condition in light_rain_weather_conditions:
+    elif weather_condition in light_rain_weather_conditions:
       print('Playing LIGHT RAIN weather condition')
       a = "light_rain.wav"
-    elif test_weather_condition in light_drizzle_weather_conditions:
+    elif weather_condition in light_drizzle_weather_conditions:
       print('Playing LIGHT DRIZZLE weather condition')
       a = "light_drizzle.wav"
-    elif test_weather_condition in drizzle_weather_conditions:
+    elif weather_condition in drizzle_weather_conditions:
       print('Playing DRIZZLE weather condition')
       a = "drizzle.wav"
-    elif test_weather_condition in heavy_drizzle_weather_conditions:
+    elif weather_condition in heavy_drizzle_weather_conditions:
       print('Playing HEAVY DRIZZLE weather condition')
       a = "heavy_drizzle.wav"
-    elif test_weather_condition in thunderstorm_weather_conditions:
+    elif weather_condition in thunderstorm_weather_conditions:
       print('Playing THUNDERSTORM weather condition')
       a = "thunderstorm.wav"
-    elif test_weather_condition in thunderstorm_rain_weather_conditions:
+    elif weather_condition in thunderstorm_rain_weather_conditions:
       print('Playing THUNDERSTORM RAIN weather condition')
       a = "thunderstorm_with_rain.wav"
-    elif test_weather_condition in cloudy_weather_conditions:
+    elif weather_condition in cloudy_weather_conditions:
       print('Playing CLOUDY weather condition')
       a = "cloudy.wav"
-    elif test_weather_condition in snow_weather_conditions:
+    elif weather_condition in snow_weather_conditions:
       print('Playing SNOW weather condition')
       a = "snow.wav"
-    elif test_weather_condition in other_weather_conditions:
+    elif weather_condition in other_weather_conditions:
       print('Playing OTHER weather condition')
       a = "other.wav"
   else:
